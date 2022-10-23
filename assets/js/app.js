eac0fab23b960477d9ead45e5e9891338f44ffa1
anasmoundir <anasmoundir1996@gmail.com>
2022-10-23 14:22:33 +0100

    part finnished

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,36 +2,43 @@
  * In this file app.js you will find all CRUD functions name.
  *
  */
-var index;
 //the form that accept the creation and display of the tasks
 var divButton = document.getElementById("to-do-tasks");
+var countToDo = document.getElementById("to-do-tasks-count");
 var inprogresse = document.getElementById("in-progress-tasks");
+var countInProgress = document.getElementById("in-progress-tasks-count");
 var donetasks = document.getElementById("done-tasks");
-var array = [];
-
-//this function is for the creation of
-function editTask(id) {}
-
+var countDoneTasks = document.getElementById("done-tasks-count");
 //the function that shows all tasks
 function showAllTasks() {
   //first of thing make all inputs empty
-
   divButton.innerHTML = "";
   inprogresse.innerHTML = "";
   donetasks.innerHTML = "";
-
+  var count = 0;
+  var count1 = 0;
+  var count2 = 0;
   //second thing we go trought a loop
   for (var i = 0; i < tasks.length; i++) {
     //index = tasks[i].length;
+    var a = document.getElementById("icone");
+    if (tasks[i].status === "In Progress") {
+      iconStr = "bi bi-hourglass-split fs-3 text-success";
+    } else if (tasks[i].status === "To Do") {
+      iconStr = "bi bi-question-circle fs-3 text-success";
+    } else if (tasks[i].status === "Done") {
+      iconStr = "bi bi-check-circle-fill fs-3 text-success";
+    }
+
     let button = `<button   data-bs-target="#exampleModal2"  data-bs-toggle="modal" class=" p-2 w-100 d-flex border-0  bg-white mb-1" onclick="getId(${i})" >
     <div class="px-3">
-        <i class="bi bi-question-circle fs-3 text-success  "></i>
+        <i class="${iconStr}" id="icone"></i>
     </div>
     <div class="text-start">
         <div class="h5">${tasks[i].title}</div>
         <div class="text-start">
             <div class="h6">#${i + 1} created in ${tasks[i].date}</div>
-            <div class="" title="">${tasks[i].description}</div>
+            <div class="description" title="${tasks[i].description}">${tasks[i].description}</div>
         </div>
         <div class="text-start">
             <span class="btn btn-primary">${tasks[i].type}</span>
@@ -41,15 +48,30 @@ function showAllTasks() {
 </button>`;
 
     if (tasks[i].status === "In Progress") {
+      iconStr = "bi bi-hourglass-split fs-3 text-success";
       inprogresse.innerHTML += button;
+      count1 += 1;
     } else if (tasks[i].status === "To Do") {
       divButton.innerHTML += button;
+      iconStr = "bi bi-question-circle fs-3 text-success";
+      count += 1;
     } else if (tasks[i].status === "Done") {
       donetasks.innerHTML += button;
+      iconStr = "bi bi-check-circle-fill fs-3 text-success";
+      count2 += 1;
     }
+    countToDo.innerText = count;
+    countInProgress.innerText = count1;
+    countDoneTasks.innerText = count2;
   }
+  document.getElementById("text-title").value = "";
+  document.getElementById("flexRadioDefault1").checked = false;
+  document.getElementById("flexRadioDefault2").value = false;
+  document.getElementById("form-select1").value = "";
+  document.getElementById("time").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("form-select2").value = "";
 }
-
 function saveTask() {
   // Recuperer task attributes a partir les champs input
   var exampleFormControlInput1 = document.getElementById("text-title");
@@ -73,10 +95,10 @@ function saveTask() {
   tasks.push(task);
   // refresh tasks
   showAllTasks();
-  console.log(tasks);
 }
-
+//get the informations from the array and show it to data.
 function getId(i) {
+  document.getElementById("id").hidden = true;
   document.getElementById("id").value = i;
   document.getElementById("text-title2").value = tasks[i].title;
   document.getElementById("form-select1.1").value = tasks[i].priority;
@@ -89,12 +111,13 @@ function getId(i) {
   return i;
 }
 
+//delete a task
 function DeleteTasks() {
   let id = document.getElementById("id").value;
   tasks.splice(id, 1);
   showAllTasks();
 }
-
+//update date
 function updateTasks() {
   let id = document.getElementById("id").value;
   tasks[id].title = document.getElementById("text-title2").value;
@@ -102,7 +125,7 @@ function updateTasks() {
   tasks[id].status = document.getElementById("form-select2.1").value;
   tasks[id].date = document.getElementById("time1").value;
   tasks[id].description = document.getElementById("description1").value;
-
-  console.log(tasks);
+  var radioBoxe1 = document.getElementById("flexRadioDefault1-1");
+  radioBoxe1.checked == true ? (tasks[id].type = "Feature") : (tasks[id].type = "Bug");
   showAllTasks();
 }
